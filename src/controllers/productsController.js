52bff5ea7bfa14b282134e32ff961699e691c72b
maf248 <maf_248@hotmail.com>
2021-02-17 16:24:46 -0300

    Completé el metodo STORE para dar de alta productos. Actualicé la vista del formulario de creación agregando ciertos inputs necesarios.

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,6 +9,10 @@ const controller = {
 		.then((products) => {
 			res.render('products', {products: products});
 		})
+		.catch(error => {
+			console.log(error);
+			res.render('error')
+		})
 	},
 
 	// Detail - Detail from one product
@@ -17,16 +21,50 @@ const controller = {
 		.then((product) => {
 			res.render('detail', {product: product, toThousand: toThousand});
 		})
+		.catch(error => {
+			console.log(error);
+			res.render('error')
+		})
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		res.render('product-create-form');
+		db.Category.findAll()
+		.then((categories) => {
+				db.Brand.findAll()
+			.then((brands) => {
+				res.render('product-create-form', {categories: categories, brands: brands});
+			})
+			.catch(error => {
+				console.log(error);
+				res.render('error')
+			})
+		})
+		.catch(error => {
+			console.log(error);
+			res.render('error')
+		})
+		
+		
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		db.Product.create({
+			title: req.body.name,
+			description: req.body.description, 
+			photo: '/images/products/' + req.file.filename,
+			price: req.body.price,
+			category_id: req.body.category,
+			brand_id: req.body.brand			
+		})
+		.then(newProduct => {
+			res.redirect('/products')
+		})
+		.catch(error => {
+			console.log(error);
+			res.render('error')
+		})
 	},
 
 	// Update - Form to edit
@@ -34,6 +72,10 @@ const controller = {
 		db.Product.findByPk(req.params.id)
 		.then((product) => {
 			res.render('product-edit-form', {productToEdit: product});
+		})
+		.catch(error => {
+			console.log(error);
+			res.render('error')
 		})
 		
 	},
