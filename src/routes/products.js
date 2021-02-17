f0ae6bd6713ef5dcd9dc0e4fabccc5c765d5bdce
maf248 <maf_248@hotmail.com>
2021-02-17 16:08:16 -0300

    Agregue multer y un input para poder subir imagenes al crear un producto. Configuré ambos de forma que funcionen

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,30 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
+
+// ************ Multer ************
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+     return cb(null, 'public/images/products');
+    },
+    filename: function(req, file, cb) {
+     return cb(null, 'producto' + '-' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({storage: storage});
 
 /*** GET ALL PRODUCTS ***/ 
 router.get('/', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/create', upload.single('productImage'), productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
