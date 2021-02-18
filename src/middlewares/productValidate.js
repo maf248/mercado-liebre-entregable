9cdab8419feff63a38670dd4d8a297ad6102f1fb
maf248 <maf_248@hotmail.com>
2021-02-18 02:26:43 -0300

    Cree el middleware "productValidate" que chequea los campos requeridos en las consignas. Además agregue los mensajes de error en la vista del formulario de creacion de productos y chequee que funcione todo correctamente

---
+++ src/middlewares/productValidate.js
@@ -0,0 +1,41 @@
+const {check, body} = require('express-validator');
+const db = require('../db/models');
+const path = require('path');
+const fs = require('fs');
+
+module.exports = [
+
+    check('name')
+    .isLength({min:3, max: 100})
+    .withMessage('Este campo debe contener entre 3 y 100 caracteres'),
+    check('price')
+    .isFloat({min: 1})
+    .withMessage('El precio debe ser mayor a 0'),
+    check('brand')
+    .isNumeric()
+    .withMessage('Por favor selecciona una marca de la lista'),
+    check('description')
+    .isLength({max:1000})
+    .withMessage('Has superado el máximo de 1.000 caracteres'),
+    body('productImage')
+    .custom(function(value, {req}) {
+        if (typeof req.file.filename != 'undefined') {
+
+            var extension = (path.extname(req.file.filename)).toLowerCase();
+            switch (extension) {
+            case '.jpg':
+                return true;
+            case '.jpeg':
+                return true;
+            case  '.png':
+                return true;
+            default:
+                return false;
+            }
+
+        }
+        return false;
+    })
+    .withMessage('Debes subir una imagen del producto (formatos permitidos: .jpg .jpeg .png)')
+    
+]
