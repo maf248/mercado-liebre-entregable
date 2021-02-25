be0bb3cd3fc6ad29018b64e7397029930e0db5b7
maf248 <maf_248@hotmail.com>
2021-02-25 01:03:25 -0300

    Cree los metodos AVATARSHOW y AVATARCHANGE en el controller principal para poder implementar el cambio de avatar. Además cree la vista basica de la ruta /avatar/upload y sus rutas correspondientes.

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,11 +1,39 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+// ************ Middleware's ************
+const routesMiddleware = require('../middlewares/routesMiddleware');
 
 // ************ Controller Require ************
 const mainController = require('../controllers/mainController');
 
+// ************ Multer ************
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+    let dirImage = path.join('public', 'images', 'users');
+    if (!fs.existsSync(dirImage)) {
+        return fs.mkdir(dirImage, error => cb(error, dirImage))
+        }
+     return cb(null, dirImage);
+    },
+    filename: function(req, file, cb) {
+           
+     return cb(null, 'Usuario-' + req.session.user.id + '_' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({storage: storage});
+
+// ************ Main Routes ************
+
 router.get('/', mainController.index);
+
+router.get('/avatar/upload', routesMiddleware, mainController.avatarShow);
+router.post('/avatar/upload', upload.any(), mainController.avatarChange);
 
 router.get('/search', mainController.search); 
 
