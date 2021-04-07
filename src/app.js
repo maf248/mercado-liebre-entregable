a3798238e714b54d90bd4911597b92adeb3b2d7a
maf248 <maf_248@hotmail.com>
2021-04-07 15:18:02 -0300

    Mejoré el carrousel de imagenes para que sea mas responsive y se vea mejor en casi todas las resoluciones.

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-app.use(session({secret: "Mensaje secreto grupo 5", resave: false, saveUninitialized: true}));
+app.use(session({secret: "Mensaje secreto Mercado Liebre MaF!", resave: false, saveUninitialized: true}));
 app.use(sessionMiddleware);
 
 // ************ Template Engine - (don't touch) ************
